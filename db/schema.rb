257ea7de8385f18460eceff9e9762f9c8b5bcf92
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-06 19:23:01 +0100

    Created pins controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105234643) do
+ActiveRecord::Schema.define(version: 20150106181510) do
+
+  create_table "pins", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
