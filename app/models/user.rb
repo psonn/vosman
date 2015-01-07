de54a403240394a8bd2f28ee0cee68fabde94ef2
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-07 01:40:08 +0100

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, uniqueness: true
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
 end
