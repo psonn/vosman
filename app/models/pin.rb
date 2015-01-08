db6b48efab02f1a45f768a9ac927111e6c21c2a7
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-08 17:15:48 +0100

    Changed the width of the pins show page

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :show => "600x600", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
     validates :image, presence: true
