256e70c276cd96185d3be815ea9fa2423dfd2d88
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-11 03:15:12 +0100

    Changed admin layout slightly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,17 @@ module ApplicationHelper
   def back_to(path)
     link_to("&laquo; Terug".html_safe, path, :class => 'btn btn-default')
   end
+
+  def wrap(content)
+    sanitize((content.split.map{ |s| wrap_long_string(s) }.join(' ')).html_safe)
+  end
+
+  private
+
+    def wrap_long_string(text, max_width = 10)
+      zero_width_space = "&#8203;"
+      regex = /.{1,#{max_width}}/
+      (text.length < max_width) ? text :
+                                  text.scan(regex).join(zero_width_space)
+    end
 end
