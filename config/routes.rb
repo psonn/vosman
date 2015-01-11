26c726b581d6145446b80465e69c8e265fa6e001
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-11 18:15:24 +0100

    Added routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
 
   resources :pins
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
   root "pages#home"
   get 'users' => 'users#index'
 
