151759ae2bb99a647831d15723af76bb3165b427
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-11 18:33:58 +0100

    Deleted registration page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :pins
 
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :skip => :registration
 
   root "pages#home"
   get 'users' => 'users#index'
