7acdb9a9a062ed93d62b61b7b97eb3acfd9b1004
Peter Sonneveld <peter_tjuuh@hotmail.com>
2015-01-19 01:03:47 +0100

    Compressed stylesheets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
+  config.assets.version = '1.1' # was '1.0'
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
